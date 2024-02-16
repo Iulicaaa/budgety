cc5168f5a0c17ebbe3061b918b769cc94df5e839
Iulia Halmaghi <iulia.halmaghi@gmail.com>
2024-02-16 14:40:36 +0200

    Add new components and update styles in Dashboard page

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,6 +1,13 @@
 import React from "react";
 import { PageLayout } from "@/components/PageLayout";
 import styled from "styled-components";
+import { Divider } from "antd";
+import {
+  PlusCircleOutlined,
+  QuestionCircleOutlined,
+  StockOutlined,
+} from "@ant-design/icons";
+import man_on_office from "@/assets/man_on_office.svg";
 
 //to do
 //folosim props cu titlu in fiecare pagina(pages) pentru a schimba titlul paginii
@@ -22,29 +29,36 @@ const Dashboard = () => {
             }}
           />
           <Content>
-            <Image src={man_with_phone} />
+            <Image src={man_on_office} />
             <p>your transactions will apear here</p>
           </Content>
         </Card1>
-        <Card2>
-          <Bar>
-            <p>Calendar</p>
-            <IconDiv>
-              <LeftCircleOutlined />
-              <RightCircleOutlined />
-            </IconDiv>
-          </Bar>
-          <Divider
-            style={{
-              padding: 0,
-              margin: 0,
-            }}
-          />
-          <Content>
-            <Image src={woman_on_laptop} />
-            <p>your monthly insights</p>
-          </Content>
-        </Card2>
+        <RightSide>
+          <Card2>
+            <Bar>
+              <p>Balane</p>
+              <QuestionCircleOutlined />
+            </Bar>
+            <Divider
+              style={{
+                padding: 0,
+                margin: 0,
+              }}
+            />
+            <Content>Dashboard Balance</Content>
+          </Card2>
+          <Card3>
+            <Content>card3</Content>
+          </Card3>
+          <Card4>
+            <Content>
+              <p>Expenses</p>
+              <p>0.00</p>
+              <p>Balance decreased</p>
+            </Content>
+            <StockOutlined />
+          </Card4>
+        </RightSide>
       </CardWrapper>
     </PageLayout>
   );
@@ -52,22 +66,88 @@ const Dashboard = () => {
 
 export default Dashboard;
 
+const Image = styled.img`
+  width: 70%;
+  height: auto;
+`;
+
+const RightSide = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  width: 40%;
+  height: 100%;
+`;
+
+const CardWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  height: 100%;
+  background-color: transparent;
+  border-radius: 8px;
+  gap: 20px;
+`;
+
 const Card1 = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  justify-content: space-between;
   width: 100%;
   height: 100%;
   background-color: white;
   border-radius: 8px;
 `;
 
+const Card2 = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  height: 60%;
+  background-color: white;
+  border-radius: 8px;
+`;
+
+const Card3 = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  height: 20%;
+  background-color: white;
+`;
+
+const Card4 = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: center;
+  width: 100%;
+  height: 20%;
+  background-color: white;
+  overflow: auto;
+`;
+
+const Bar = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  padding: 0 24px;
+  height: 64px;
+  background-color: transparent;
+  font-size: 14px;
+  font-weight: 500;
+  color: #000;
+  margin-bottom: 0;
+  padding-bottom: 0;
+`;
+
 const Content = styled.div`
   display: flex;
   flex-direction: column;
-  justify-content: center;
-  align-items: flex-start;
+  align-items: center;
   width: 80%;
   max-width: 610px;
   padding-top: 42px;
